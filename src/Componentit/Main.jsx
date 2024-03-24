3b3adff955980d35d4b25feb0d7edce49cda42ce
GORDEX000 <ezatullahnuurzaii@outlook.com>
2024-03-25 00:30:40 +0200

    edit 1.0

---
+++ src/Componentit/Main.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
+
+const Main = () => {
+    return (
+        <div id='main' className='flex justify-center items-center h-screen'>
+            <img className='absolute inset-0 w-full h-full object-cover object-left' src="https://images.unsplash.com/photo-1565043666747-69f6646db940?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <div className='absolute inset-0 flex justify-center items-center bg-white/50'>
+                <div className='text-center'>
+                    <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Ra Vuokraus</h1>
+                    <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
+                        Paras{' '}
+                        <TypeAnimation
+                            sequence={[
+                                'Vuokraus',
+                                1000,
+                                'hullu',
+                                1000,
+                                'crazy',
+                                1000,
+                            ]}
+                            wrapper="div"
+                            cursor={true}
+                            repeat={Infinity}
+                            style={{ fontSize: '1em', paddingLeft: '5px' }}
+                        />
+                    </h2>
+                    <div className='flex justify-center pt-6 max-w-[200] w-full'>
+                        <FaTwitter className='cursor-pointer mr-4' size={20} />
+                        <FaFacebook className='cursor-pointer mr-4' size={20} />
+                        <FaInstagram className='cursor-pointer' size={20} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Componentit/NavBar.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { AiOutlineMenu, AiOutlineHome, AiOutlineMail } from 'react-icons/ai';
+import { MdCarRental } from "react-icons/md";
+import { IoPerson } from "react-icons/io5";
+
+
+const NavBar = () => {
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+    };
+
+    return (
+        <div>
+            <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden' />
+            {nav ? (
+                <div className='fixed w-full h-screen bg-white/90 flex-col justify-center items-center z-20'>
+                    <a href="#home"
+                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineHome size={20} />
+                        <span className='pl-4'>Home</span>
+                    </a>
+                    <a href="#vuokra"
+                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <MdCarRental size={20} />
+                        <span className='pl-4'>vuokra</span>
+                    </a>
+                    <a href="#mail"
+                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <AiOutlineMail size={20} />
+                        <span className='pl-4'>Ota Yhteytta</span>
+                    </a>
+                    <a href="#tekijat"
+                        className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+                        <IoPerson size={20} />
+                        <span className='pl-4'>Tekijat</span>
+                    </a>
+                </div>
+            ) : (
+                ''
+            )}
+            <div className='md:block hidden fixed top-[25%] z-10'>
+                <div className='flex flex-col'>
+                    <a href="#home" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursore-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineHome size={20}/>
+                    </a>
+                    <a href="#vuokra" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursore-pointer hover:scale-110 ease-in duration-300'>
+                        <MdCarRental size={20}/>
+                    </a>
+                    <a href="#mail" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursore-pointer hover:scale-110 ease-in duration-300'>
+                        <AiOutlineMail size={20}/>
+                    </a>
+                    <a href="#tekijat" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursore-pointer hover:scale-110 ease-in duration-300'>
+                        <IoPerson size={20}/>
+                    </a>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+    );
+};
+
+export default NavBar;
