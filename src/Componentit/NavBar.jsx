16e15aab22d9ee44908797df24b60115344d3ccc
GORDEX000 <ezatullahnuurzaii@outlook.com>
2024-03-26 23:11:53 +0200

    nav got some small update

--- src/Componentit/NavBar.jsx
+++ src/Componentit/NavBar.jsx
@@ -15,22 +15,22 @@ const NavBar = () => {
             <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden' />
             {nav ? (
                 <div className='fixed w-full h-screen bg-white/90 flex-col justify-center items-center z-20'>
-                    <a href="#home"
+                    <a onClick={handleNav} href="#main"
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineHome size={20} />
                         <span className='pl-4'>Home</span>
                     </a>
-                    <a href="#vuokra"
+                    <a onClick={handleNav} href="#vuokra"
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <MdCarRental size={20} />
                         <span className='pl-4'>vuokra</span>
                     </a>
-                    <a href="#mail"
+                    <a onClick={handleNav} href="#mail"
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <AiOutlineMail size={20} />
                         <span className='pl-4'>Ota Yhteytta</span>
                     </a>
-                    <a href="#tekijat"
+                    <a onClick={handleNav} href="#tekijat"
                         className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                         <IoPerson size={20} />
                         <span className='pl-4'>Tekijat</span>
@@ -41,7 +41,7 @@ const NavBar = () => {
             )}
             <div className='md:block hidden fixed top-[25%] z-10'>
                 <div className='flex flex-col'>
-                    <a href="#home" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursore-pointer hover:scale-110 ease-in duration-300'>
+                    <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursore-pointer hover:scale-110 ease-in duration-300'>
                         <AiOutlineHome size={20}/>
                     </a>
                     <a href="#vuokra" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursore-pointer hover:scale-110 ease-in duration-300'>
