e0340db056a3d2a6bfa7ad6bdeebf8c2d19b4797
GORDEX000 <ezatullahnuurzaii@outlook.com>
2024-03-28 00:30:23 +0200

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './index.css';
 import Main from './Componentit/Main';
 import Vuokra from './Componentit/Vuokra';
 import Mail from './Componentit/Mail';
-
+import Footer from './Componentit/Footer';
+import AboutSite from './Componentit/AboutSite';
+import Map from './Componentit/Map';
 
 function App() {
 
@@ -14,6 +16,9 @@ function App() {
     <Main />
     <Vuokra />
     <Mail />
+    <Map/>
+    <AboutSite />
+    <Footer />
     </>
   )
 }
---
+++ src/Componentit/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='text-center bg-slate-600'>
+            <div className='pt-8 pb-8'>
+                <p><a href="tel:PHONE_NUM">050 560 2822</a> / myynti@ra-vuokraus.com</p>
+                <p>TURUN RA-VUOKRAUS OY</p>
+                <p>Puistomäenkatu 57</p>
+                <p>20810 Turku</p>
+                <br />
+                <p>20810 Turku</p>
+                <a href="tel:PHONE_NUM">+358 50 560 2822</a>
+                <p>Tiedustelut 08:30-19:00, autojen nouto Puistomäenkadulta 08:30-16:00</p>
+                <p>Lauantaina - Sunnuntaina ja pyhäpäivät auki sopimuksen mukaan.</p>
+                <a href="tel:PHONE_NUM">050 560 2822</a>
+                <br />
+                <br />
+                <br />
+                <br />
+                <p>Copyright 2024</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
